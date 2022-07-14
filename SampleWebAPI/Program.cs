868d9701ea9a5862f305d1440c8fcf9b2b7405c1
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-07-14 13:32:27 +0700

    menambahkan interface

--- SampleWebAPI/Program.cs
+++ SampleWebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SampleWebAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//menambahkan konfigurasi EF
+builder.Services.AddDbContext<SamuraiContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("SamuraiConnection")).EnableSensitiveDataLogging());
 
 var app = builder.Build();
 
